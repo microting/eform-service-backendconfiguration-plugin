da3dd23d2d96badb092bdaa8a5f7db345d57c76d
René Schultz Madsen <rm@microting.dk>
2025-01-19 09:09:45 +0100

    Fixing tests.

--- ServiceBackendConfigurationPlugin.Integration.Test/CanaryInAColeMine.cs
+++ ServiceBackendConfigurationPlugin.Integration.Test/CanaryInAColeMine.cs
@@ -32,7 +32,7 @@ namespace ServiceBackendConfigurationPlugin.Integration.Test
         [Test]
         public void CanPeep()
         {
-            Assert.True(true);
+            Assert.That(true);
         }
     }
 }
